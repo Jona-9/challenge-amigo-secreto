642e8af1b44d9311a4c1fb6b28c653583519d386
Jonathan <jonathantl09s@gmail.com>
2025-02-27 23:32:50 -0500

    primera función del código

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function avisosdecambioslet(tipo, nuevoname){
+  nombre = document.querySelector(tipo);
+  nombre.innerHTML=nuevoname;
+}
+// Agregué una función para notificar sobre algún cambio que se produzca más adelante
+avisosdecambioslet("h2","Hola Jona");
