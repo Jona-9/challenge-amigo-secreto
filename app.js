23a9cbf8bb7c8e91b80abd3a107a0e7f10fb3402
Jonathan Tuppia Lozano <jonathantl09s@gmail.com>
2025-03-06 19:42:29 -0500

    nuevasFuncionesagregadas

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
 let amigos = [];
-function avisosdecambios(tipo, nuevoname){
+let nuevoname;
+function avisosdecambios(tipo,  nuevoname){
   nombre = document.querySelector(tipo);
   nombre.innerHTML=nuevoname;
 }
 // Agregué una función para notificar sobre algún cambio que se produzca más adelante
 // avisosdecambios("h2","Hola Jona");
 function agregarAmigo(){
+let CantidadAmigos = document.querySelector("ul");
+let li=document.createElement("li");
   avisosdecambios("h2","Digite el nombre de sus amigos")
   let amigo = document.querySelector("input").value;
- if(amigo === "" || (/^\d+$/.test(amigo))){
+ if (amigo === "" || (/^\d+$/.test(amigo))){
   avisosdecambios("h2","Error! Digite el nombre de sus amigos correctamente")
- }else{
+ } else {
   amigos.push(amigo);
+  li.textContent=amigo; 
+ CantidadAmigos.appendChild(li);
  }
  limpiarCaja();
  console.log(amigos);
 }
-/* He creado una función para almacenar los valores y puse una 
+ /*He creado una función para almacenar los valores y puse una 
 condición, para que solo entren los que sean tipo Texto */
-
-console.log(amigos.length)
-
 function limpiarCaja(){
   let limpiar = document.getElementById('amigo');
   limpiar.value = "";
   return;
+} 
+ let aleatorio = Math.floor(Math.random()*amigos.length);
+ let arrayAuxiliar= [];
+
+ function sortearAmigo(){
+let CantidadAmigos = document.querySelector("ul");
+let li=document.createElement("li");
+  console.log(amigos.length)
+   aleatorio = Math.floor(Math.random()*amigos.length);
+  console.log(aleatorio);
+ /* if (arrayAuxiliar.length==amigos.length){
+    avisosdecambios("h2"," Felicidades acabaste el sorteo!")
+  } else if (arrayAuxiliar.includes(aleatorio)){
+    return sortearAmigo();
+    } else { */
+     if(arrayAuxiliar.length < amigos.length){
+    do{
+      aleatorio = Math.floor(Math.random()*amigos.length);
+      }while(arrayAuxiliar.includes(aleatorio));
+      li.textContent=(`El amigo secreto sorteado es ${amigos[aleatorio]}`); 
+      CantidadAmigos.appendChild(li);
+       arrayAuxiliar.push(aleatorio);
+    }else{
+      avisosdecambios("h2"," Felicidades acabaste el sorteo!")
+    }
+    
 }
-/* habilitar el otro botón y crear números aleatorios del tamaño 
-de la matriz y desabilitar el botón añadir, para evitar erorres*/
+/*do{
+aleatorio = Math.floor(Math.random()*amigos.length);
+}while(arrayAuxiliar.includes(aleatorio)){
+
+}  */
+//habilitar el otro botón y crear números aleatorios del tamaño 
+ // de la matriz y desabilitar el botón añadir, para evitar erorres
